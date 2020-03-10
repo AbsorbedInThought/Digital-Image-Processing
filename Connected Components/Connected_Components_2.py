3c16251fec66819275b67362d92722b51caa2906
Muhammad Taha <37846193+AbsorbedInThought@users.noreply.github.com>
2020-03-10 23:51:17 +0500

    Update and rename task3.py to Connected_Components_2.py

--- Connected Components/Connected_Components_2.py
+++ Connected Components/Connected_Components_2.py
@@ -1,17 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 31 21:50:30 2019
-
-@author: Taha
-"""
-
 import cv2 as cv
 
 ##################
 #Common Tweaks
 NEIGHBORS = 8
 THRESH_VAL = 127
-IMAGE_NAME = "circuit.png"
+IMAGE_NAME = "readImage.jpg"
 #################
 
 img = cv.imread(IMAGE_NAME, 0)
@@ -27,12 +20,11 @@ color_shift = int(255/no_of_objects)
 color_list = [0]*no_of_objects
 
 for i in range(0, no_of_objects):
-    color_list[i] = i*color_shift
-    
+    color_list[i] = i*color_shift  
     
 for i in range(0, rows):
     for j in range(0, cols):
             color_value = color_list[objects[i][j]]
             objects[i][j] = color_value
         
-cv.imwrite("output.png", objects)
+cv.imwrite("output.jpg", objects)
